56180d8b3255004b437c9058d71adda58abaccac
Nora Delaney <nora.delaney@gmail.com>
2015-08-25 14:02:40 -0400

    wip

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,11 +1,27 @@
-# class Board
-#   attr_accessor :
-#
-# end
+class Board
+  attr_reader :grid
+  def initialize(input = {})
+    @grid = input.fetch(:grid, default_grid)
+  end
+
+  private
+
+  def default_grid
+    Array.new()
+  end
+end
 
 class Cell
   attr_accessor :value
   def initialize(value = "")
     @value = value
+  end
+end
+
+class Player
+  attr_accessor :color, :name
+  def initiaize(input)
+    @color = input.fetch(:color)
+    @name = input.fetch(:name)
   end
 end
