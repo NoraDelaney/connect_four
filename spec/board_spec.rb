6be3c0bd6b1cbf43c0ab63fb79e0a0b15607db82
Nora Delaney <nora.delaney@gmail.com>
2015-08-25 14:44:14 -0400

    wip

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,4 +7,22 @@ describe Board do
       expect { Board.new(grid: "grid") }.to_not raise_error
     end
   end
+
+  it "sets the grid with six rows by default" do
+    board = Board.new
+    expect(board.grid).to have(6).things
+  end
+
+  it "creates seven things in each row by default" do
+    board = Board.new
+    board.grid.each do |row|
+      expect(row).to have(6).things
+    end
+  end
+
+  context "#grid" do
+    it "returns the grid" do
+      expect()
+    end
+  end
 end
