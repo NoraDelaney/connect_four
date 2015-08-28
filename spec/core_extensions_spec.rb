f0e88902ac39a6aa6103e89052bd6ada67606a33
Nora Delaney <nora.delaney@gmail.com>
2015-08-28 18:15:48 -0400

    wip

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -10,8 +10,22 @@ describe Array do
       expect(["", 1, "", "", "", "", "", Object.new, :a].all_empty?).to be_falsey
     end
 
-    it "returns true for an empty Array"
+    it "returns true for an empty Array" do
       expect([].all_empty?).to be_truthy
+    end
+  end
+
+  context "#all_same?" do
+    it "returns true if all elements of the Array are the same" do
+      expect(["A", "A", "A"].all_same?).to be_truthy
+    end
+
+    it "returns false if some of the Array elements are different" do
+      expect(["", 1, "", Object.new, :a].all_same?).to be_false
+    end
+
+    it "returns true for an empty Array" do
+      expect([].all_same?).to be_true
     end
   end
 end
